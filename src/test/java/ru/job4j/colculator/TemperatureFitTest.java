3ff8dfc80b555ef931308b5fd4b60e889e55fe46
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-26 01:45:11 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/test/java/ru/job4j/colculator/TemperatureFitTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.colculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TemperatureFitTest {
+    @Test
+    void whenRoomTemperature10ThenFruits8() {
+        int input = 10;
+        double expectedResult = 8.0;
+        double actualResult = TemperatureFit.idealTemperatureForFruits(input);
+        assertThat(actualResult).isEqualTo(expectedResult, withPrecision(0.01));
+    }
+
+    @Test
+    void whenRoomTemperature10ThenMeat5() {
+        int input = 10;
+        double expectedResult = 5.0;
+        double actualResult = TemperatureFit.idealTemperatureForMeat(input);
+        assertThat(actualResult).isEqualTo(expectedResult, withPrecision(0.01));
+    }
+}
