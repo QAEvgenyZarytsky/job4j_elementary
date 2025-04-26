7699a3a1ca6ca8431e1310177e58a491f9827067
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-27 02:22:00 +0300

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean result = true;
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if ((number % i == 0)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
