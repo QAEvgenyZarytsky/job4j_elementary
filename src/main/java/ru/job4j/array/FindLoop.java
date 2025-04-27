58efdd973473577bce8bac0438c215c683112e35
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-27 22:12:51 +0300

    Поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,6 +1,25 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        if (data.length == 0) {
+            System.out.println("Error: data.length = 0!");
+        } else if (start > data.length || finish > data.length) {
+            System.out.println("Error: start or finish more then data.length!");
+        } else if (start > finish) {
+            System.out.println("Error: start more then finish!");
+        } else {
+            for (int index = start; index < data.length - (data.length - 1 - finish); index++) {
+                if (data[index] == element) {
+                    result = index;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
     public static int indexOf(int[] data, int element) {
         int result = -1;
         for (int index = 0; index < data.length; index++) {
