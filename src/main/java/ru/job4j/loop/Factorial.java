54bc5df5a38575aebd2b33c795be51079681f46b
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-28 10:40:30 +0300

    5.2. Создать программу, вычисляющую факториал. [Клон] [#505211 #541426]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -2,13 +2,13 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calculate(int number) {
-    if (number < 0) {
-        throw new IllegalArgumentException("Number can not be negative value.");
-    }
-    int result = 1;
-    for (int i = 2; i <= number; i++) {
-        result *= i;
-    }
-    return  result;
+        if (number < 0) {
+            throw new IllegalArgumentException("Number can not be negative value.");
+        }
+        int result = 1;
+        for (int i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
