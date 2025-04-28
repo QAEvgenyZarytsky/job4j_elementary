1a04c3779db4322a4949580b03cfd249c5d43635
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-28 10:53:10 +0300

    Слово начинается с ... [#505233 #541517]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,15 +3,10 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        if (prefix.length > word.length) {
-            System.out.println("Error: prefix.length more then world.length!");
-            result = false;
-        } else {
-            for (int index = 0; index < prefix.length; index++) {
-                if (word[index] != prefix[index]) {
-                    result = false;
-                    break;
-                }
+        for (int index = 0; index < prefix.length; index++) {
+            if (word[index] != prefix[index]) {
+                result = false;
+                break;
             }
         }
         return result;
