2ce2761eece53e6c6fcb3120f66b6515d28cac42
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-29 23:14:39 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,11 +10,9 @@ public class ArrayDefinition {
         names[1] = "Petr";
         names[2] = "Alex";
         names[3] = "Sam";
-
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
-
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -4,7 +4,6 @@ public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
         int j = array.length - 1;
-
         while (i < j) {
             int sum = array[i] + array[j];
             if (sum == target) {
